73329c3c77037c9d21916d11f26267a650479454
Ananias <99740075+Anan1as@users.noreply.github.com>
2023-10-13 18:07:21 -0400

    Listas listas

--- premios.py
+++ premios.py
@@ -1,7 +1,12 @@
+# Crear grupos A, B y C con coderas y sus datos personales(Nombre, Mes de Ingreso, Grupo y Edad).
+grupoAs = [("Maria Aurora Riquelme", "Febrero", "3A", 20), ("Ricard Poveda", "Febrero", "3A", 22), ("Iker Palau", "Marzo", "3A", 21), ("Naroa Vegas", "Marzo", "4A", 21)]
+grupoBs = [("Camila Romero", "Febrero", "3B", 20), ("Marcelo Padron", "Febrero", "4B", 32), ("Cristina Maria Urbano", "Marzo", "4B", 29), ("Carmelo Lorente", "Abril", "4B", 28)]
+grupoCs = [("Ruth Vegas", "Abril", "3C", 26), ("Roberto Miguez", "Abril", "3C", 30), ("Maria Sonia Lago", "Abril", "4C", 27), ("Emiliano Ponce", "Abril", "4C", 29)]
 
+print (grupoAs[0], grupoBs[3], grupoCs[1])
 '''
 #Funcionalidades del módulo de mantenimiento de CODERS:
-- Crear grupos A, B y C con coderas y sus datos personales(Nombre, Mes de Ingreso, Grupo y Edad).
+- Crear grupos A, B y C con coders y sus datos personales(Nombre, Mes de Ingreso, Grupo y Edad).
 - Registrar Trainer del grupo A.
 - Buscar Coders duplicados en 2 grupos.
 - Eliminar Coders por inasistencia.
